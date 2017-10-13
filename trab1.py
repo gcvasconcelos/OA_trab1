ae20fe53afa7cc44d1981e3c874279a9dbef8a45
Gabriel Vasconcelos <gabrielvasconcelos@cjr.org.br>
2017-10-13 15:50:41 -0300

    Arquivos secundários implementados com lista invertida

--- trab1.py
+++ trab1.py
@@ -1,50 +1,59 @@
 def printa_arquivo(arquivo):
-	# print('MATRIC\tNOME\t\t\t\tOP\tCURSO\tTURMA')
-	dados_arquivo = []
-	for linha in arquivo:
-		dados = linha.split()
-		tamanho_nome = len(' '.join(dados[1:-3]))
-		tabs = '\t\t'
-		if tamanho_nome >= 24:
-			tabs = '\t'
-		# print(dados[0] + '\t' + ' '.join(dados[1:-3]) + tabs + dados[-3] + '\t' + dados[-2] + '\t' + dados[-1])
-		dados_arquivo.append(dados)
-	return dados_arquivo
+    # print('MATRIC\tNOME\t\t\t\tOP\tCURSO\tTURMA')
+    dados_arquivo = []
+    for linha in arquivo:
+        dados = linha.split()
+        tamanho_nome = len(' '.join(dados[1:-3]))
+        tabs = '\t\t'
+        if tamanho_nome >= 24:
+            tabs = '\t'
+        # print(dados[0] + '\t' + ' '.join(dados[1:-3]) + tabs + dados[-3] + '\t' + dados[-2] + '\t' + dados[-1])
+        dados_arquivo.append(dados)
+    return dados_arquivo
 
-def cria_indices(indices, arquivo):
-	arquivo.seek(0,0)
-	referencia = 0
-	for linha in arquivo:
-		dados = linha.split()
-		nome = ' '.join(dados[1:-3])
-		matricula = dados[0]
-		chave_primaria = matricula + '$' + nome
-		indices.write(chave_primaria[0:30] + '\t' + str(referencia) + '\n')
-		print(chave_primaria[0:30] + '\t' + str(referencia) + '\n')
-		referencia += 1
-	return
+def cria_indices(dados, indices):
+    referencia = 0
+    linhas = []
+    chaves_primarias = []
+    for i in range(len(dados)):
+        matricula = str(dados[i][0])
+        nome = ''.join(dados[i][1:-3])
+        chave_primaria = matricula + '$' + nome
+        chaves_primarias.append(chave_primaria)
+        linhas.append(chave_primaria[0:30] + '\t' + str(referencia) + '\n')
+        referencia += 1
+    indices.writelines(linhas)
+    return chaves_primarias
 
-def printa_indices(indices):
-	print('CP\t\t\t\tREF')
-	for linha in indices:
-		print linha
-	return
-
-def cria_indices_secundarios(op, turma, primario, dados):
-	for i in range(len(dados)):
-		op.write(dados[i][-1] + '\n')
-		turma.write(dados[i][-3] + '\n')
-	return
+def cria_indices_secundarios(arq_op, arq_turma, primaria, dados):
+    ops = []
+    turmas = []
+    HEAD_op = [-1]
+    HEAD_turma = [-1, -1]
+    for i in range(len(dados)):
+        if dados[i][-3] not in ops:
+            ops.append(dados[i][-3])
+        if dados[i][-1] not in turmas:
+            turmas.append(dados[i][-1])
+    for i in range(len(dados)):
+        if dados[i][-3] in ops:
+            arq_op.write(primaria[i] + '\t' + str(HEAD_op[0]) + '\n')
+            HEAD_op[0] = i
+        if dados[i][-1] in turmas:
+            arq_turma.write(primaria[i] + '\t' + str(HEAD_turma[turmas.index(dados[i][-1])]) + '\n')
+            HEAD_turma[turmas.index(dados[i][-1])] = i
+    return
 
 
 lista1 = open('benchmarks/lista1.txt', 'r')
 dados_arquivo = printa_arquivo(lista1)
-indices1 = open('indice_lista1.ind', 'w+')
-cria_indices(indices1, lista1)
-# printa_indices(indices1)
+indices1 = open('indice_lista1.ind', 'w')
+pks = cria_indices(dados_arquivo, indices1)
 secundario_op = open('op_lista1.ind', 'w+')
 secundario_turma = open('turma_lista1.ind', 'w+')
-cria_indices_secundarios(secundario_op, secundario_turma, indices1, dados_arquivo)
+cria_indices_secundarios(secundario_op, secundario_turma, pks, dados_arquivo)
 
 lista1.close()
 indices1.close()
+secundario_op.close()
+secundario_turma.close()
