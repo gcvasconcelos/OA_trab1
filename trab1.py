7b88562df6a2d13cc269446400df91d1f1f6abea
Gabriel Vasconcelos <gabrielvasconcelos@cjr.org.br>
2017-10-14 00:20:09 -0300

    Refatorei o código, bem mais legivel

--- trab1.py
+++ trab1.py
@@ -1,59 +1,62 @@
-def printa_arquivo(arquivo):
-    # print('MATRIC\tNOME\t\t\t\tOP\tCURSO\tTURMA')
-    dados_arquivo = []
+def inicializa_registros(arquivo):
+    registros = []
     for linha in arquivo:
+        registro = {}
         dados = linha.split()
-        tamanho_nome = len(' '.join(dados[1:-3]))
-        tabs = '\t\t'
-        if tamanho_nome >= 24:
-            tabs = '\t'
-        # print(dados[0] + '\t' + ' '.join(dados[1:-3]) + tabs + dados[-3] + '\t' + dados[-2] + '\t' + dados[-1])
-        dados_arquivo.append(dados)
-    return dados_arquivo
+        registro['matric'] = dados[0]
+        registro['nome'] = ' '.join(dados[1:-3])
+        registro['op'] = dados[-3]
+        registro['curso'] = dados[-2]
+        registro['turma'] = dados[-1]
+        registros.append(registro)
+    return registros
 
-def cria_indices(dados, indices):
-    referencia = 0
-    linhas = []
-    chaves_primarias = []
-    for i in range(len(dados)):
-        matricula = str(dados[i][0])
-        nome = ''.join(dados[i][1:-3])
-        chave_primaria = matricula + '$' + nome
-        chaves_primarias.append(chave_primaria)
-        linhas.append(chave_primaria[0:30] + '\t' + str(referencia) + '\n')
-        referencia += 1
-    indices.writelines(linhas)
-    return chaves_primarias
+def inicializa_indices(arq_indices, registros):
+    indices = []
+    posicao = 0
+    for registro in registros:
+        indice = {}
+        chave_primaria = registro['matric'] + '#' + registro['nome']
+        chave_primaria = chave_primaria[0:30]
+        indice['pk'] = chave_primaria
+        indice['posicao'] = posicao
+        indices.append(indice)
+        posicao += 1
+        arq_indices.write(indice['pk'] + '\t' + str(indice['posicao']) + '\n')
+    return indices
 
-def cria_indices_secundarios(arq_op, arq_turma, primaria, dados):
-    ops = []
-    turmas = []
-    HEAD_op = [-1]
-    HEAD_turma = [-1, -1]
-    for i in range(len(dados)):
-        if dados[i][-3] not in ops:
-            ops.append(dados[i][-3])
-        if dados[i][-1] not in turmas:
-            turmas.append(dados[i][-1])
-    for i in range(len(dados)):
-        if dados[i][-3] in ops:
-            arq_op.write(primaria[i] + '\t' + str(HEAD_op[0]) + '\n')
-            HEAD_op[0] = i
-        if dados[i][-1] in turmas:
-            arq_turma.write(primaria[i] + '\t' + str(HEAD_turma[turmas.index(dados[i][-1])]) + '\n')
-            HEAD_turma[turmas.index(dados[i][-1])] = i
+def opcoes_secundario(registros, ind_secundario):
+    opcoes = []
+    for registro in registros:
+        if registro[ind_secundario] not in opcoes:
+            opcoes.append(registro[ind_secundario])
+    return opcoes
+
+def inicializa_indice_secundario(arq_secundario, registros, indices, ind_secundario):
+    opcoes = opcoes_secundario(registros, ind_secundario)
+    head = []
+    for i in range(len(opcoes)):
+        head.append(-1)
+    i = 0
+    for registro in registros:
+        if registro[ind_secundario] in opcoes:
+            foo = opcoes.index(registro[ind_secundario])
+            arq_secundario.write(indices[i]['pk'] + '\t' + registro[ind_secundario] + '\t' + str(head[foo]) + '\n')
+            head[foo] = indices[i]['posicao']
+        i += 1
     return
 
-
 lista1 = open('benchmarks/lista1.txt', 'r')
-dados_arquivo = printa_arquivo(lista1)
-indices1 = open('indice_lista1.ind', 'w')
-pks = cria_indices(dados_arquivo, indices1)
+registros = inicializa_registros(lista1)
+lista1.close()
+
+indices1 = open('indice_lista1.ind', 'w+')
+indices = inicializa_indices(indices1, registros)
+indices1.close()
+
 secundario_op = open('op_lista1.ind', 'w+')
 secundario_turma = open('turma_lista1.ind', 'w+')
-cria_indices_secundarios(secundario_op, secundario_turma, pks, dados_arquivo)
-
-lista1.close()
-indices1.close()
+inicializa_indice_secundario(secundario_op, registros, indices, 'op')
+inicializa_indice_secundario(secundario_turma, registros, indices, 'turma')
 secundario_op.close()
 secundario_turma.close()
